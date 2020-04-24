2db5e7b42513a0050b9acf6ee273b0d72db4314f
yannispetro <yannispetro@users.noreply.github.com>
2020-04-23 21:11:52 -0400

    exclude notebooks

--- urbanculture.py
+++ urbanculture.py
@@ -43,7 +43,7 @@ def index():
     if form.validate_on_submit():
         city = form.city_form.data
         kws = form.keywords_form.data.replace(',','').split()
-        print(kws)
+
         dfc = pd.read_pickle('models/' + city + '_dfc.pkl.gz', compression='gzip')
         dfn = pd.read_pickle('models/' + city + '_dfn.pkl.gz', compression='gzip')
         dfc = recom.keywords_score(kws, dfc, city)
@@ -61,14 +61,15 @@ def keywords_score(kws, dfc, city):
     cs_airbnb = lambda x: cosine_similarity(x.vec_airbnb,kws_vec_airbnb)[0][0]
     dfc['kws_score_airbnb'] = dfc.apply(cs_airbnb, axis=1)
     dfc['kws_score_airbnb'] = dfc['kws_score_airbnb']/dfc['kws_score_airbnb'].sum()
-
+    #
     kws_vec_reviews = tfidf_reviews.transform([kws])
     cs_reviews = lambda x: cosine_similarity(x.vec_reviews,kws_vec_reviews)[0][0]
     dfc['kws_score_reviews'] = dfc.apply(cs_reviews, axis=1)
     dfc['kws_score_reviews'] = dfc['kws_score_reviews']/dfc['kws_score_reviews'].sum()
-
+    #
     dfc['kws_score'] = dfc['kws_score_airbnb'] + dfc['kws_score_reviews']
     dfc['kws_score'] = dfc['kws_score']/dfc['kws_score'].max()
+    # dfc['kws_score'] = dfc['kws_score_airbnb']
 
     dfc['alpha'] = 0.01 + (dfc['kws_score'] - dfc['kws_score'].min())/(dfc['kws_score'].max() - dfc['kws_score'].min())*0.59
 
