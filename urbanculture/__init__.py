43b742ba6de53c5e379fae4f46a6adeb8499df1a
yannispetro <yannispetro@users.noreply.github.com>
2020-04-24 14:46:46 -0400

    added Procfile

--- urbanculture/__init__.py
+++ urbanculture/__init__.py
@@ -20,7 +20,7 @@ def create_app(config_file='settings.py'):
 
     return app
 
-app = create_app()
+# app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from urbanculture import create_app
+
+app = create_app()
