f1a2d888a1d381910bbfc1ba3b22e6311bc3996a
yannispetro <yannispetro@users.noreply.github.com>
2020-04-24 08:55:18 -0400

    modified database to include date and remote IP address

--- urbanculture/models.py
+++ urbanculture/models.py
@@ -2,5 +2,7 @@ from .extensions import db
 
 class SearchQuery(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(20))
+    ip_address = db.Column(db.String(20))
     city = db.Column(db.String(50))
     keywords = db.Column(db.String(300))
--- urbanculture/routes.py
+++ urbanculture/routes.py
@@ -1,6 +1,7 @@
+import time
 import pandas as pd
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_wtf import Form
 from wtforms import StringField, SelectField
 from wtforms.validators import InputRequired, AnyOf
@@ -12,7 +13,14 @@ import urbanculture.recommendations as recom
 
 main = Blueprint('main', __name__)
 
+exclude = ['antwerp','asheville','barossa-valley','barwon-south-west-vic',
+           'beijing','belize','bergamo','bristol','broward-county',
+           'cambridge','clark-county-nv','columbus','denver','euskadi','ghent',
+           'new-brunswick','northern-rivers','ottawa','pacific-grove','portland',
+           'rhode-island','salem-or','tasmania','trentino','twin-cities-msa','vaud']
+
 df_url = pd.read_csv('data/allCitiesData.csv.gz', compression='gzip')
+df_url = df_url[~df_url.city.isin(exclude)]
 cities_alias = list(df_url.city)
 cities_names = list(df_url.city_clean)
 cities_alias = ['Select a city'] + cities_alias
@@ -35,10 +43,16 @@ class CityForm(Form):
 def index():
     form = CityForm()
     if form.validate_on_submit():
+        date = time.strftime('%Y-%m-%d %H:%M:%S')
+        ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
         city = form.city_form.data
-        kws = form.keywords_form.data.replace(',','').split()
+        keywords = form.keywords_form.data
 
-        p = recom.get_plot_handle(city,kws)
+        searchquery = SearchQuery(date=date, ip_address=ip_address, city=city, keywords=keywords)
+        db.session.add(searchquery)
+        db.session.commit()
+
+        p = recom.get_plot_handle(city, keywords.replace(',','').split() )
 
         script, div = components(p)
         return render_template("graph.html", script=script, div=div)
