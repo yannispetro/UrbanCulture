a8d4daa96bf1ed748c16454d61cff3bd6c9c0482
yannispetro <yannispetro@users.noreply.github.com>
2020-05-10 14:10:36 -0400

    changed map colormap and limited recommendations to top10

--- urbanculture/recommendations.py
+++ urbanculture/recommendations.py
@@ -47,7 +47,10 @@ def keywords_score(kws, dfc, city):
     dfc['kws_score'] = dfc['kws_score'] - dfc['kws_score'].min()
     dfc['kws_score_norm'] = dfc['kws_score']/dfc['kws_score'].max()
 
-    dfc['alpha'] = 0.05 + (dfc['kws_score_norm'] - dfc['kws_score_norm'].min())/(dfc['kws_score_norm'].max() - dfc['kws_score_norm'].min())*0.7
+    threshold = min(heapq.nlargest(10, list(dfc['kws_score_norm'])))
+    dfc['kws_score_norm'] = dfc['kws_score_norm'].apply(lambda x: 0 if x < threshold else x)
+
+    dfc['alpha'] = 0.1 + (dfc['kws_score_norm'] - dfc['kws_score_norm'].min())/(dfc['kws_score_norm'].max() - dfc['kws_score_norm'].min())*0.7
     dfc['alpha'].fillna(0.1,inplace=True)
 
     return dfc
